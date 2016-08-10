ed79eed523e02bd7e812ea2d0643caefb857cde1
Vjacheslav V. Borisov <slavb18@gmail.com>
2016-08-10 13:09:43 +0000

    DescriptorUtils + Slf4jSessionLogger
    git-svn-id: https://svn.ilb.ru/repos/common/trunk@62 a5b2bf95-3cba-4c8e-be42-a0ce6bbb2e66

---
+++ common-jpa/src/main/java/ru/ilb/common/jpa/tools/DescriptorUtils.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.ilb.common.jpa.tools;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import org.eclipse.persistence.descriptors.ClassDescriptor;
+import org.eclipse.persistence.mappings.DatabaseMapping;
+import org.eclipse.persistence.mappings.OneToManyMapping;
+import org.eclipse.persistence.mappings.OneToOneMapping;
+import org.eclipse.persistence.sessions.Session;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+/**
+ *
+ * @author slavb
+ */
+public class DescriptorUtils {
+
+    EntityManager entityManager;
+
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public void fixInverseLinks(Object entity) {
+        final BeanWrapper src = new BeanWrapperImpl(entity);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+        ClassDescriptor cd = entityManager.unwrap(Session.class).getDescriptor(entity);
+        if (cd == null) {
+            return;
+        }
+        for (java.beans.PropertyDescriptor pd : pds) {
+            DatabaseMapping dm = cd.getMappingForAttributeName(pd.getName());
+            if (dm != null) {
+                if (dm instanceof OneToManyMapping) {
+                    OneToManyMapping dmOtM = (OneToManyMapping) dm;
+                    if (dmOtM.getMappedBy() != null) {
+                        List srcValue = (List) src.getPropertyValue(pd.getName());
+                        if (srcValue != null) {
+                            for (Object v : srcValue) {
+                                final BeanWrapper srcv = new BeanWrapperImpl(v);
+                                srcv.setPropertyValue(dmOtM.getMappedBy(), entity);
+                                fixInverseLinks(v);
+                            }
+                        }
+
+                    }
+                }
+                if (dm instanceof OneToOneMapping) {
+                    OneToOneMapping dmOtO = (OneToOneMapping) dm;
+                    if (dmOtO.getMappedBy() != null) {
+                        Object srcValue = src.getPropertyValue(pd.getName());
+                        if (srcValue != null) {
+                            final BeanWrapper srcv = new BeanWrapperImpl(srcValue);
+                            srcv.setPropertyValue(dmOtO.getMappedBy(), entity);
+                            fixInverseLinks(srcValue);
+                        }
+
+                    }
+                }
+            }
+
+        }
+    }
+
+}
