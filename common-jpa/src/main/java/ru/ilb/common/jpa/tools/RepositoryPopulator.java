665d242f1231698fa9db62aeac9fd7ae974a6463
Vjacheslav V. Borisov <slavb18@gmail.com>
2017-04-07 04:51:51 +0000

    AutoPopulableRepository
    git-svn-id: https://svn.ilb.ru/repos/common/trunk@106 a5b2bf95-3cba-4c8e-be42-a0ce6bbb2e66

---
+++ common-jpa/src/main/java/ru/ilb/common/jpa/tools/RepositoryPopulator.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2017 slavb.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ru.ilb.common.jpa.tools;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import ru.ilb.common.jpa.annotations.AutoPopulableRepository;
+
+/**
+ * Automate JPA-repository population by public static object instances
+ * Usage: 
+ * 1. Register &lt;bean class="ru.ilb.common.jpa.tools.RepositoryPopulator"/>
+ * 2. Add @AutoPopulableRepository annotation to JPA Repository
+ * 3. Add static entity instances to JPA Repository, e.g.
+ * public static PointType OFFICE = new PointType(1L, PointTypeCode.OFFICE, "Office");
+ * @author slavb
+ */
+@Component
+public class RepositoryPopulator {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @PostConstruct
+    public void populateAll() {
+        applicationContext.getBeansWithAnnotation(AutoPopulableRepository.class).values()
+                .stream().forEach(repository -> populateRepository((JpaRepository) repository));
+
+    }
+
+    public void populateRepository(JpaRepository repository) {
+        Class repositoryInterface = (Class) repository.getClass().getGenericInterfaces()[0];
+        ParameterizedType baseInterface = ((ParameterizedType) repositoryInterface.getGenericInterfaces()[0]);
+        Type objectType = baseInterface.getActualTypeArguments()[0];
+
+        //JpaRepository repository = applicationContext.getBean(repositoryClass);
+        List objects = Arrays.stream(repositoryInterface.getDeclaredFields())
+                .filter(field -> java.lang.reflect.Modifier.isStatic(field.getModifiers()))
+                .filter(field -> field.getGenericType().equals(objectType))
+                .map(field -> getFieldValue(field))
+                .collect(Collectors.toList());
+        repository.save(objects);
+    }
+
+    private static Object getFieldValue(Field field) {
+        try {
+            return field.get(null);
+        } catch (IllegalArgumentException | IllegalAccessException ex) {
+            return null;
+        }
+    }
+
+}
