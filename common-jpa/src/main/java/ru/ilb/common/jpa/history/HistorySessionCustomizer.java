2bde864b3e25e8adf0ebe5a4ed1a1b9547aee1c9
Vjacheslav V. Borisov <slavb18@gmail.com>
2017-04-11 08:38:17 +0000

    AutoHistory
    git-svn-id: https://svn.ilb.ru/repos/common/trunk@109 a5b2bf95-3cba-4c8e-be42-a0ce6bbb2e66

---
+++ common-jpa/src/main/java/ru/ilb/common/jpa/history/HistorySessionCustomizer.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2017 slavb.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ru.ilb.common.jpa.history;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.eclipse.persistence.config.SessionCustomizer;
+import org.eclipse.persistence.descriptors.ClassDescriptor;
+import org.eclipse.persistence.sessions.Session;
+import org.springframework.core.annotation.AnnotationUtils;
+
+/**
+ * Calls HistoryCustomizer.customize on all entities with @AutoHistory annotation
+ * Usage: persistance.xml 
+ * &lt;property name="eclipselink.session.customizer" value="ru.ilb.common.jpa.history.HistorySessionCustomizer"/>
+ * @author slavb
+ */
+public class HistorySessionCustomizer implements SessionCustomizer {
+
+    @Override
+    public void customize(Session session) throws Exception {
+        HistoryCustomizer hc = new HistoryCustomizer();
+
+        List<ClassDescriptor> descriptors = session.getDescriptors().values().stream()
+                .filter(d -> AnnotationUtils.getAnnotation(d.getJavaClass(), AutoHistory.class) != null)
+                .collect(Collectors.toList());
+        descriptors.stream().forEach(d -> hc.customize(d));
+    }
+
+}
