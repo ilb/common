9a20331c288e42943d019ad5a4d00ead0fd1147c
slavb <slavb@ilb.ru>
2017-08-29 07:10:12 +0000

    move LazyAccessorFactoryImpl to separate package

--- common-jpa/src/main/java/ru/ilb/common/jpa/tools/EclipseLinkUtils.java
+++ common-jpa/src/main/java/ru/ilb/common/jpa/tools/EclipseLinkUtils.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ru.ilb.common.jpa.jaxb;
+package ru.ilb.common.jpa.tools;
 
 import org.eclipse.persistence.indirection.IndirectContainer;
 
