d2c495b9b773dc847f201d09f6cbe2abf96d859b
slavb18 <slavb18@gmail.com>
2018-12-26 14:15:53 +0400

    configure WebApplicationExceptionHandler for logging cascade exceptions

--- common-jaxrs/src/main/java/ru/ilb/common/jaxrs/exceptionhandler/WebApplicationExceptionHandler.java
+++ common-jaxrs/src/main/java/ru/ilb/common/jaxrs/exceptionhandler/WebApplicationExceptionHandler.java
@@ -15,87 +15,132 @@
  */
 package ru.ilb.common.jaxrs.exceptionhandler;
 
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+import javax.xml.bind.JAXBContext;
 import org.apache.cxf.jaxrs.impl.ResponseImpl;
 import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.PhaseInterceptorChain;
+import ru.ilb.common.jaxrs.jaxb.JaxbMashaller;
 
 /**
- *
+ * To catch cascade jax-rs client exceptions, set property
+ * &lt;entry key="support.wae.spec.optimization" value="false"/>
  * @author slavb
+ *
  */
 @Provider
 public class WebApplicationExceptionHandler implements ExceptionMapper<WebApplicationException> {
 
+    @Context
+    private ContextResolver<JAXBContext> jaxbContextResolver;
+
     private static final Logger LOG = Logger.getLogger(WebApplicationExceptionHandler.class.getName());
 
     private final String UNCLASSIFIABLE_SERVER_ERROR_TEXT = "Unclassifiable server error";
-    private final int CLIENT_WEB_ERROR_START = 450;
-    private final int CLIENT_ERROR_LOW_CODE = 100;
+    private final int CLIENT_HTTP_ERROR = 450;
+    private final int CASCADE_HTTP_ERROR = 555;
+
+    private static final String KEY_EXCHANGE_ID = "exchangeId";
 
     @Override
     public Response toResponse(WebApplicationException ex) {
-        // код ответа
-        Response r = ex.getResponse();
+        // http response code
+        Response response = ex.getResponse();
         int responseStatus;
-        if (r != null) {
-            responseStatus = r.getStatus();
+        if (response != null) {
+            responseStatus = response.getStatus();
         } else {
             responseStatus = Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
         }
+        Message currentMessage = PhaseInterceptorChain.getCurrentMessage();
+        String exchangeId = currentMessage != null && currentMessage.getExchange() != null ? (String) currentMessage.getExchange().get(KEY_EXCHANGE_ID) : "(no current exchange)";
 
-        // текст ошибки
-        String outMess = ex.getMessage();
+        // message should be shown to client
+        StringBuilder message = new StringBuilder();
+        // additional message should be logged
+        StringBuilder logstr = new StringBuilder();
+
         try {
-            if (r != null && r.getEntity() != null) {
-                outMess = r.readEntity(String.class);
-            }
-            if (ex instanceof ClientErrorException && r instanceof ResponseImpl) {
-                Message m = ((ResponseImpl) r).getOutMessage();
-                if (m != null) {
-                    if (m.get("org.apache.cxf.request.uri") != null) {
-                        outMess += System.lineSeparator() + "Request URI: " + m.get("org.apache.cxf.request.uri");
+            // reparse error response if exists
+            if (response != null) {
+                Message outMessage = null;
+                // cascade exception from jax-rs client
+                boolean cascade = false;
+                if (response instanceof ResponseImpl) {
+                    outMessage = ((ResponseImpl) response).getOutMessage();
+                    if (outMessage != null) {
+                        cascade = Boolean.TRUE.equals(outMessage.get("org.apache.cxf.client"));
                     }
                 }
+                // cascade exception should be masked
+                if (cascade) {
+                    message.append("Cascade http error ").append(responseStatus);
+                    message.append(" from ").append(outMessage.get("org.apache.cxf.request.uri"));
+
+                    // original response should be logged, not shown to client
+                    if (response.getEntity() != null) {
+                        logstr.append("Cascade response :")
+                                .append(response.readEntity(String.class));
+                    }
+                    //replace proxied http code
+                    responseStatus = CASCADE_HTTP_ERROR;
+
+                    if (jaxbContextResolver!=null && outMessage.get("java.util.List") != null) {
+                        //log request object
+                        List list = (List) outMessage.get("java.util.List");
+                        for (Object object : list) {
+                            logstr.append("\nObject: ").append(JaxbMashaller.marshal(jaxbContextResolver.getContext(object.getClass()), object, MediaType.APPLICATION_XML));
+                        }
+                    }
+
+                } else {
+                    if (response.getEntity() != null) {
+                        message.append(response.readEntity(String.class));
+                    }
+                }
+            } else {
+                message.append(ex.getMessage());
             }
         } catch (Throwable ex_) {
             LOG.log(Level.SEVERE, "error on getting  addinional exception info", ex_);
         }
-        if (outMess == null || outMess.isEmpty()) {
-            outMess = ex.toString();
+
+        if (message.length() == 0) {
+            message.append(UNCLASSIFIABLE_SERVER_ERROR_TEXT);
+            logstr.append(ex.toString());
         }
 
-        LOG.log(Level.WARNING, outMess, ex);
+        message.append("\nExchange Id: ").append(exchangeId);
+        if (logstr.length() != 0) {
+            logstr.append("\n");
+        }
+        logstr.append(message);
+
+        LOG.log(Level.WARNING, logstr.toString(), ex);
+        String outMess = message.toString();
 
         // транслируем код ответа
         Response.StatusType status;
-        String outMessClient;
-        if ((responseStatus >= Response.Status.INTERNAL_SERVER_ERROR.getStatusCode() || responseStatus < CLIENT_ERROR_LOW_CODE)
-                // 551 ошибку пропускаем и транслируем текст, как есть
-                && responseStatus != 551) {
-            // ошибки до 100 и после (вкл) 500 траслируем в 500
-            status = Response.Status.INTERNAL_SERVER_ERROR;
-            outMessClient = Response.Status.INTERNAL_SERVER_ERROR.getReasonPhrase();
-        } else {
-            if (ex.getCause() != null && ex.getCause() instanceof java.lang.IllegalArgumentException
-                    && responseStatus == Response.Status.NOT_FOUND.getStatusCode()) {
-                // если 404 из-за не корректно введеного аргумента в URL, то транслируем в 450
-                outMessClient = UNCLASSIFIABLE_SERVER_ERROR_TEXT;
-                status = new CustomResponseStatus(CLIENT_WEB_ERROR_START, outMessClient);
-            } else {
-                // всё остальное - как есть
-                outMessClient = outMess;
-                status = new CustomResponseStatus(responseStatus, outMessClient);
-            }
 
+        if (ex.getCause() != null && ex.getCause() instanceof java.lang.IllegalArgumentException
+                && responseStatus == Response.Status.NOT_FOUND.getStatusCode()) {
+            // если 404 из-за не корректно введеного аргумента в URL, то транслируем в 450
+            outMess = UNCLASSIFIABLE_SERVER_ERROR_TEXT;
+            status = new CustomResponseStatus(CLIENT_HTTP_ERROR, outMess);
+        } else {
+            // всё остальное - как есть
+            status = new CustomResponseStatus(responseStatus, outMess);
         }
 
-        return Response.status(status).entity(outMessClient).header("Content-Type", MediaType.TEXT_PLAIN + ";charset=UTF-8").build();
+        return Response.status(status).entity(outMess).header("Content-Type", MediaType.TEXT_PLAIN + ";charset=UTF-8").build();
     }
 }
